49631c33bff8b0fae675ffb8052c9cca2dac9c99
Eugene Debrah <debraheug@gmail.com>
2022-03-22 10:47:19 +0000

    Shared Faker : Async Script Loading

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,6 +1,1 @@
-import faker from "faker";
-
-const cart = faker.random.number();
-const cartText = `<div>You have ${cart} items in your cart </div>`;
-
-document.querySelector("#cart-dev").innerHTML = cartText;
+import("./bootstrap");
